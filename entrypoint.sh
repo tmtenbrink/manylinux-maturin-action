4678b55a1d19f995bff7dfc3883b40fff9cdcfcf
tmtenbrink <64082573+tmtenbrink@users.noreply.github.com>
2021-09-21 12:25:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh -l
 
-cd $GITHUB_WORKSPACE
-echo $(pwd)
-echo "I did something!"
-mkdir /build
-cd /build
->hello.text
-echo $(ls)
 cd /github/home
-mkdir /build
-cd /build
+mkdir /buildb
+cd /buildb
 >hello.text
 echo $(ls)
